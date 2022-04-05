d010c6e87bb32b8ccf5c34957d96f3b80a9fb7ac
aditya <agoswami0708g@gmail.com>
2022-04-05 23:40:36 +0530

    done

--- block-BJaadg/code/index.js
+++ block-BJaadg/code/index.js
@@ -7,15 +7,17 @@ let to = 'Arya Stark';
 /*
 1. Find the index of the first 'is' in the variable quote. And store it in a new variable named indexOfIs
 */
-
+let indexOfIs = quote.indexOf(`is`);
+console.log(indexOfIs);
 /*
 2. Find the character at the index indexOfIs (Problem 1) in quote.
 */
+console.log(quote.charAt(indexOfIs));
 
 /*
 3. Log the message saying `The index of first is in quote is 7`
 */
-
+console.log(`The index of first is in quote is `+ quote.indexOf("s"));
 /*
 4. Log the message for first 6 characters of quote like this.
   The character at index 0 is 'T'
@@ -25,36 +27,49 @@ let to = 'Arya Stark';
   The character at index 4 is 'e'
   The character at index 5 is ' '
 */
+console.log(quote.charAt(0));
+console.log(quote.charAt(1));
+console.log(quote.charAt(2));
+console.log(quote.charAt(3));
+console.log(quote.charAt(4));
+console.log(quote.charAt(5));
 
 /*
 5. Using the variable from , to and quote variable dispaly this message
   "Syrio Forel said There is only one thing we say to death: Not today to Arya Stark." (use concat method)
 */
-
+console.log(from.concat(quote,to));
 /*
 6. Does from, to and quote ends with "rk". Check all three.
 */
-
+if (from.endsWith("rk") && to.endsWith("rk") && quote.endsWith("rk")){
+  console.log("true");
+}
+else{
+  console.log("False");
+}
 /*
 7. Does quote includes the word "Only"
 */
-
+console.log(quote.includes("Only"));
 /*
 8. Does quote includes the word " we"
 */
-
+console.log(quote.includes("we"));
 /*
 9. Find the index of the the word `we` in quote
 */
-
+console.log(quote.indexOf("we"));
 /*
 10. Split the quote into individual word and store it in a variable name quoteSplitted
 */
-
+let quoteSplitted = quote.split(" ");
+console.log(quoteSplitted);
 /*
 11. Change the word "today" in quoteSplitted to "tomorrow" and join all the words to form a sentance.
 */
-
+let splittrd = quoteSplitted.replace(/"today","tomorrow");
+console.log(splittrd)
 /*
 12. Find the index of second "o" in quote. Use indexOf
 */
